8a832f4f2b3eae64ca02e9aec5fbd4c3577bc1fa
celestecarolinaortiz <ccov1804@gmail.com>
2026-01-02 14:26:09 +0100

    Agrego arreglos

--- src/App.java
+++ src/App.java
@@ -1,5 +1,35 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        /*ARREGLLOS: Estructuras de datos que permiten almacenar múltiples valores en una sola variable
+        Se puede acceder a ellos a través de un índice que empieza en 0 
+        También conocidos como Vectores o Matrices en otros lenguajes */
+
+        int[] numeros = new int[5]; //Declaración e instanciación de un arreglo de enteros con 5 posiciones
+        numeros[0] = 100;
+        numeros[1] = 200;
+        numeros[2] = 400;
+        numeros[3] = 500;
+        numeros[4] = 600;
+
+        System.out.println("Elemento en la posición 2: " + numeros[2]); 
+        System.out.println("Longitud del array: " + numeros.length);
+
+        for(int i=0; i<numeros.length; i++){
+            System.out.println(numeros[i]);
+        }
+
+        //Declaración, instanciación y asignación de valores en una sola línea 
+        String[] alfabeto = {"A", "B", "C", "D", "E"};  
+       
+         //For mejorado para recorrer arreglos. Sin índice
+         for(String letra : alfabeto){ 
+            System.out.println(letra);
+         }
+
+
+
+        /*EJEMPLO DE PROGRAMACIÓN ORIENTADA A OBJETOS*/
+
        //Instanciamos un objeto Persona usando el primer constructor
         Persona persona1 = new Persona("Celeste", "Ortiz", 25, "TIC", 5, true);
 
@@ -16,7 +46,7 @@ public class App {
         System.out.println("Edad: " + persona2.getEdad());
         System.out.println("Carrera: " + persona2.carrera.nombre);
       
-
+        
 
     }
 }
